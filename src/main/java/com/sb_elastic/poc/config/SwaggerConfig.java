e95cc32b2193eb12c29bbf68defd46a834cdddec
Erik Finnman <erik.finnman@layer10.se>
2024-10-25 13:58:18 +0200

    Add Swagger docs

---
+++ src/main/java/com/sb_elastic/poc/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.sb_elastic.poc.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("sb-elastic-poc")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SB Elastic Poc API"));
+    }
+}

95b1363d9c96729fd2ea40fffdf5eaba95d24c74
Erik Finnman <erik.finnman@layer10.se>
2024-10-25 10:46:28 +0200

    Spring Data query along with custom query

--- src/main/java/com/sb_elastic/poc/storage/repositories/PersonRepository.java
+++ src/main/java/com/sb_elastic/poc/storage/repositories/PersonRepository.java
@@ -1,7 +1,24 @@
 package com.sb_elastic.poc.storage.repositories;
 
 import com.sb_elastic.poc.storage.entities.PersonDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends ListCrudRepository<PersonDocument, Long> {
+    List<PersonDocument> findPersonDocumentByFirstName(String firstName);
+
+    @Query("""
+            {
+              "wildcard": {
+                "lastName": {
+                  "value": "?0",
+                  "boost": 1.0,
+                  "rewrite": "constant_score"
+                }
+              }
+            }
+            """)
+    List<PersonDocument> findPersonDocumentByLastNameWildcard(String lastName);
 }
--- src/test/java/com/sb_elastic/poc/storage/repositories/PersonRepositoryTest.java
+++ src/test/java/com/sb_elastic/poc/storage/repositories/PersonRepositoryTest.java
@@ -3,6 +3,8 @@ package com.sb_elastic.poc.storage.repositories;
 import com.sb_elastic.poc.TestcontainersConfiguration;
 import com.sb_elastic.poc.storage.entities.PersonDocument;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.elasticsearch.DataElasticsearchTest;
 import org.springframework.context.annotation.Import;
@@ -19,13 +21,41 @@ class PersonRepositoryTest {
     @Test
     void testStorePerson() {
         var person = new PersonDocument();
-        person.setFirstName("Erik");
-        person.setLastName("Finnman");
+        person.setFirstName("Some");
+        person.setLastName("Person");
 
         var savedPerson = personRepository.save(person);
 
         assertThat(savedPerson.getId()).isNotNull();
-        assertThat(savedPerson.getFirstName()).isEqualTo("Erik");
-        assertThat(savedPerson.getLastName()).isEqualTo("Finnman");
+        assertThat(savedPerson.getFirstName()).isEqualTo("Some");
+        assertThat(savedPerson.getLastName()).isEqualTo("Person");
     }
+
+    @Test
+    void testQueryFirstName() {
+        var person = new PersonDocument();
+        person.setFirstName("Some");
+        person.setLastName("Person");
+        personRepository.save(person);
+
+        var persons = personRepository.findPersonDocumentByFirstName("Some");
+
+        assertThat(persons.getFirst().getFirstName()).isEqualTo("Some");
+        assertThat(persons.getFirst().getLastName()).isEqualTo("Person");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"*innman", "*nnman", "*man"})
+    void testQueryLastNameContaining(String lastName) {
+        var person = new PersonDocument();
+        person.setFirstName("Erik");
+        person.setLastName("Finnman");
+        personRepository.save(person);
+
+        var persons = personRepository.findPersonDocumentByLastNameWildcard(lastName);
+
+        assertThat(persons.getFirst().getLastName()).isEqualTo("Finnman");
+    }
+    
+    
 }
